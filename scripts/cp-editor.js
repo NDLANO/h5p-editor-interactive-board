789771882eaa33585f8ec6c218696aa2d89ca39f
EivinS <eivsel@WPF1W2VPJ.localdomain>
2021-06-24 07:55:29 +0200

    making textbox size depend on content in the textbox

--- scripts/cp-editor.js
+++ scripts/cp-editor.js
@@ -2298,6 +2298,44 @@ H5PEditor.CoursePresentation.prototype.showElementForm = function (element, $wra
             }
           }
         }
+        const isAdvancedText = machineName === 'H5P.AdvancedText';
+        if(isAdvancedText) {
+          if(elementParams.width == this.defaultElementWidthOfContainerInPercent) {
+            const text = elementParams.action.params.text;
+            const lengthText = text.length;
+            const numberOfParagraphs = text.split('<p>').length - 1;
+            const numberOfListElements = text.split('<li>').length - 1;
+            const elementAspectRatio = this.getDefaultElementAspectRatio('H5P.AdvancedText');
+            // console.log(`paragraphs and listE: ${numberOfParagraphs + numberOfListElements}`);
+            // console.log(lengthText);
+            // console.log(text);
+
+            if(numberOfParagraphs + numberOfListElements == 1 && lengthText < 150) {
+              elementParams.width = this.defaultElementWidthOfContainerInPercent;
+              elementParams.height = (elementParams.width * trueSlideAspectRatio / elementAspectRatio) / 4;
+            }
+            else if(numberOfParagraphs + numberOfListElements == 1 && lengthText >= 150 && lengthText < 300) {
+              elementParams.width = this.defaultElementWidthOfContainerInPercent;
+              elementParams.height = (elementParams.width * trueSlideAspectRatio / elementAspectRatio) / 2;
+            }
+            else if(numberOfParagraphs + numberOfListElements == 1 && lengthText >= 300) {
+              elementParams.width = this.defaultElementWidthOfContainerInPercent;
+              elementParams.height = (elementParams.width * trueSlideAspectRatio / elementAspectRatio);
+            }
+            else if(numberOfParagraphs + numberOfListElements == 2 && lengthText < 300 ) {
+              elementParams.width = this.defaultElementWidthOfContainerInPercent;
+              elementParams.height = (elementParams.width * trueSlideAspectRatio / elementAspectRatio) / 2;
+            }
+            else if(numberOfParagraphs + numberOfListElements == 2 && lengthText >= 300) {
+              elementParams.width = this.defaultElementWidthOfContainerInPercent;
+              elementParams.height = (elementParams.width * trueSlideAspectRatio / elementAspectRatio);
+            }
+            else if(numberOfParagraphs + numberOfListElements > 2) {
+              elementParams.width = this.defaultElementWidthOfContainerInPercent;
+              elementParams.height = (elementParams.width * trueSlideAspectRatio / elementAspectRatio);
+            }
+          }
+        }
         this.redrawElement($wrapper, element, elementParams);
       });
     }
